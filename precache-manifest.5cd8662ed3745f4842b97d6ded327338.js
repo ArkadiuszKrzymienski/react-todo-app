f6f88cfda617a0f14cca1089d380bd9af3e3c1a9
Arkadiusz Krzymienski <a.krzymienski@gmail.com>
2021-04-14 17:50:12 +0200

    Updates

---
+++ precache-manifest.5cd8662ed3745f4842b97d6ded327338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a1f98f473028ddb2e2b",
+    "url": "/react-todo-app/static/css/main.e5f5849e.chunk.css"
+  },
+  {
+    "revision": "2a1f98f473028ddb2e2b",
+    "url": "/react-todo-app/static/js/main.a3f6eb1c.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  },
+  {
+    "revision": "4890904c536a35fb968a",
+    "url": "/react-todo-app/static/js/2.f23bfe8f.chunk.js"
+  },
+  {
+    "revision": "e140929bf4f601b0d186072297d00fb7",
+    "url": "/react-todo-app/index.html"
+  }
+];
